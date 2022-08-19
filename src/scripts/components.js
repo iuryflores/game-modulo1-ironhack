71f07816de9fbf10ec17efc3a95c37740059f7db
Iury H. Flores <iury.flores@gmail.com>
2022-08-19 07:41:56 -0300

    Game ready

--- src/scripts/components.js
+++ src/scripts/components.js
@@ -1,136 +1,135 @@
 class Component {
-  constructor(x, y, width, height, personagemImg) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.personagemImg = personagemImg;
-    const livesImg = new Image();
-    livesImg.src = "./src/imgs/live.png";
-  }
+    constructor(x, y, width, height, personagemImg) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.personagemImg = personagemImg;
 
-  draw() {
-    const ctx = myGame.context;
-    ctx.drawImage(personagemImg, this.x, this.y, this.width, this.height);
-  }
-  drawLives() {
-    const ctx = myGame.context;
-    ctx.font = "30px serif";
-    ctx.fillStyle = "blue";
-    ctx.drawImage(livesImg, this.x, this.y, 30, 20);
-    /*ctx.fillText(`❤`, this.x, this.y);*/
-  }
+    }
 
-  top() {
-    return this.y;
-  }
-  bottom() {
-    return this.y + this.height;
-  }
-  left() {
-    return this.x;
-  }
-  right() {
-    return this.x + this.width;
-  }
-  crashWith(obstacle) {
-    return !(
-      this.bottom() < obstacle.top() ||
-      this.top() > obstacle.bottom() ||
-      this.right() < obstacle.left() ||
-      this.left() > obstacle.right()
-    );
-  }
-  crashWithLives(power) {
-    return !(
-      this.bottom() < power.top() ||
-      this.top() > power.bottom() ||
-      this.right() < power.left() ||
-      this.left() > power.right()
-    );
-  }
-  updateLives() {
-    if (myGame.frames % 1200 === 0) {
-      let posY = Math.floor(Math.random() * myGame.canvas.height + 100);
-      myGame.powers.push(new Component(myGame.canvas.width, posY, 20, 20));
+    draw() {
+        const ctx = myGame.context;
+        ctx.drawImage(personagemImg, this.x, this.y, this.width, this.height);
     }
-    for (power of myGame.powers) {
-      power.x -= myGame.speed;
-      power.drawLives();
+    drawLives() {
+        const ctx = myGame.context;
+        ctx.font = "30px serif";
+        ctx.fillStyle = "blue";
+        ctx.drawImage(livesImg, this.x, this.y, 30, 20);
+        /*ctx.fillText(`❤`, this.x, this.y);*/
     }
-  }
+
+    top() {
+        return this.y;
+    }
+    bottom() {
+        return this.y + this.height;
+    }
+    left() {
+        return this.x;
+    }
+    right() {
+        return this.x + this.width;
+    }
+    crashWith(obstacle) {
+        return !(
+            this.bottom() < obstacle.top() ||
+            this.top() > obstacle.bottom() ||
+            this.right() < obstacle.left() ||
+            this.left() > obstacle.right()
+        );
+    }
+    crashWithLives(power) {
+        return !(
+            this.bottom() < power.top() ||
+            this.top() > power.bottom() ||
+            this.right() < power.left() ||
+            this.left() > power.right()
+        );
+    }
+    updateLives() {
+        if (myGame.frames % 1200 === 0) {
+            let posY = Math.floor(Math.random() * myGame.canvas.height + 100);
+            myGame.powers.push(new Component(myGame.canvas.width, posY, 20, 20));
+        }
+        for (power of myGame.powers) {
+            power.x -= myGame.speed;
+            power.drawLives();
+        }
+    }
 }
 let power = new Component();
 class Obstacle {
-  constructor(x, y, vilanImg) {
-    this.x = x;
-    this.y = y;
-    this.width = 100;
-    this.height = 150;
-    this.vilanImg = vilanImg;
-  }
-  top() {
-    return this.y;
-  }
-  bottom() {
-    return this.y + this.height;
-  }
-  left() {
-    return this.x;
-  }
-  right() {
-    return this.x + this.width;
-  }
-  drawObst() {
-    const ctx = myGame.context;
-    ctx.drawImage(vilanImg, this.x, this.y, this.width, this.height);
-  }
+    constructor(x, y, vilanImg) {
+        this.x = x;
+        this.y = y;
+        this.width = 100;
+        this.height = 150;
+        this.vilanImg = vilanImg;
+    }
+    top() {
+        return this.y;
+    }
+    bottom() {
+        return this.y + this.height;
+    }
+    left() {
+        return this.x;
+    }
+    right() {
+        return this.x + this.width;
+    }
+    drawObst() {
+        const ctx = myGame.context;
+        ctx.drawImage(vilanImg, this.x, this.y, this.width, this.height);
+    }
 
-  updateObstacle() {
-    if (myGame.frames < 600) {
-      if (myGame.frames % 300 === 0) {
-        let posY = Math.floor(Math.random() * myGame.canvas.height - 150);
-        if (posY < 0) {
-          posY = 0;
-        } else if (posY >= myGame.canvas.height) {
-          posY = myGame.canvas.height - 150;
+    updateObstacle() {
+        if (myGame.frames < 600) {
+            if (myGame.frames % 300 === 0) {
+                let posY = Math.floor(Math.random() * myGame.canvas.height - 150);
+                if (posY < 0) {
+                    posY = 0;
+                } else if (posY >= myGame.canvas.height) {
+                    posY = myGame.canvas.height - 150;
+                }
+                myGame.obstacles.push(
+                    new Obstacle(myGame.canvas.width + 50, posY, vilanImg)
+                );
+            }
         }
-        myGame.obstacles.push(
-          new Obstacle(myGame.canvas.width + 50, posY, vilanImg)
-        );
-      }
-    }
-    if (myGame.frames > 600 && myGame.frames < 2000) {
-      if (myGame.frames % 200 === 0) {
-        let posY = Math.floor(Math.random() * (myGame.canvas.height - 150));
-        if (posY < 0) {
-          posY = 0;
-        } else if (posY >= myGame.canvas.height) {
-          posY = myGame.canvas.height - 150;
+        if (myGame.frames > 600 && myGame.frames < 2000) {
+            if (myGame.frames % 200 === 0) {
+                let posY = Math.floor(Math.random() * (myGame.canvas.height - 150));
+                if (posY < 0) {
+                    posY = 0;
+                } else if (posY >= myGame.canvas.height) {
+                    posY = myGame.canvas.height - 150;
+                }
+                myGame.obstacles.push(
+                    new Obstacle(myGame.canvas.width + 50, posY, vilanImg)
+                );
+            }
         }
-        myGame.obstacles.push(
-          new Obstacle(myGame.canvas.width + 50, posY, vilanImg)
-        );
-      }
-    }
-    if (myGame.frames > 2000) {
-      if (myGame.frames % 150 === 0) {
-        let posY = Math.floor(Math.random() * (myGame.canvas.height - 150));
-        if (posY < 0) {
-          posY = 0;
-        } else if (posY >= myGame.canvas.height) {
-          posY = myGame.canvas.height - 150;
+        if (myGame.frames > 2000) {
+            if (myGame.frames % 150 === 0) {
+                let posY = Math.floor(Math.random() * (myGame.canvas.height - 150));
+                if (posY < 0) {
+                    posY = 0;
+                } else if (posY >= myGame.canvas.height) {
+                    posY = myGame.canvas.height - 150;
+                }
+                myGame.obstacles.push(
+                    new Obstacle(myGame.canvas.width + 50, posY, vilanImg)
+                );
+            }
         }
-        myGame.obstacles.push(
-          new Obstacle(myGame.canvas.width + 50, posY, vilanImg)
-        );
-      }
+        for (obstacle of myGame.obstacles) {
+            obstacle.x -= myGame.speed;
+            obstacle.drawObst();
+
+        }
     }
-    for (obstacle of myGame.obstacles) {
-      obstacle.x -= myGame.speed;
-      obstacle.drawObst();
-      
-    }
-  }
 }
 let obstacle = new Obstacle();
--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -6,124 +6,124 @@ const vilanImg = new Image();
 vilanImg.src = "./src/imgs/ww.png";
 
 const livesImg = new Image();
-livesImg.src = "/src/imgs/live.png";
+livesImg.src = "./src/imgs/live.png";
 
 const myGame = {
-  canvas: document.querySelector("canvas"),
-  frames: 0,
-  obstacles: [],
-  stop: false,
-  player: null,
-  level: 1,
-  speed: 1,
-  score: 0,
-  lives: 5,
-  powers: [],
-  audio: new Audio("theme80.mp3"),
+    canvas: document.querySelector("canvas"),
+    frames: 0,
+    obstacles: [],
+    stop: false,
+    player: null,
+    level: 1,
+    speed: 1,
+    score: 0,
+    lives: 5,
+    powers: [],
+    audio: new Audio("theme80.mp3"),
 
-  start: function () {
-    this.player = new Component(120, 100, 100, 120, personagemImg);
+    start: function() {
+        this.player = new Component(120, 100, 100, 120, personagemImg);
 
-    this.canvas.width = 800;
-    this.canvas.height = 500;
-    this.context = this.canvas.getContext("2d");
-    this.audio.play();
-    this.audio.loop = true;
+        this.canvas.width = 800;
+        this.canvas.height = 500;
+        this.context = this.canvas.getContext("2d");
+        this.audio.play();
+        this.audio.loop = true;
 
-    startGame();
-  },
-  clear: function () {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  },
+        startGame();
+    },
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
 
-  updateScore: function () {
-    if (this.frames % (3 * this.level) === 0) {
-      this.score += 1;
-    }
+    updateScore: function() {
+        if (this.frames % (3 * this.level) === 0) {
+            this.score += 1;
+        }
 
-    this.context.font = "20px GameFont";
-    this.context.fillStyle = "black";
-    this.context.fillText(`Score: ${this.score}`, 650, 50);
-  },
-  showLives: function () {
-    let posFirst = 100;
-    this.context.font = "20px GameFont";
-    this.context.fillText(`Lives: `, 20, 50);
-    for (let i = 0; i < this.lives; i++) {
-      this.context.font = "30px serif";
-      this.context.fillStyle = "black";
-      this.context.drawImage(livesImg, posFirst, 20, 40, 30);
-      /*this.context.fillText(`❤`, posFirst, 50);*/
-      posFirst += 40;
-    }
-    if (this.lives === 5) {
-      this.context.font = "10px serif";
-      this.context.fillStyle = "black";
-      this.context.fillText(`max`, 300, 40);
-    }
-  },
+        this.context.font = "20px GameFont";
+        this.context.fillStyle = "black";
+        this.context.fillText(`Score: ${this.score}`, 650, 50);
+    },
+    showLives: function() {
+        let posFirst = 100;
+        this.context.font = "20px GameFont";
+        this.context.fillText(`Lives: `, 20, 50);
+        for (let i = 0; i < this.lives; i++) {
+            this.context.font = "30px serif";
+            this.context.fillStyle = "black";
+            this.context.drawImage(livesImg, posFirst, 20, 40, 30);
+            /*this.context.fillText(`❤`, posFirst, 50);*/
+            posFirst += 40;
+        }
+        if (this.lives === 5) {
+            this.context.font = "10px serif";
+            this.context.fillStyle = "black";
+            this.context.fillText(`max`, 300, 40);
+        }
+    },
 };
 
 function startGame() {
-  //limpando a tela
-  myGame.clear();
-  document.getElementById("game-intro").style.display = "none";
-  //desenhando o jogador
-  myGame.player.draw();
+    //limpando a tela
+    myGame.clear();
+    document.getElementById("game-intro").style.display = "none";
+    //desenhando o jogador
+    myGame.player.draw();
 
 
 
-  //desenhado/atualizando os vilões
-  obstacle.updateObstacle();
-  power.updateLives();
+    //desenhado/atualizando os vilões
+    obstacle.updateObstacle();
+    power.updateLives();
 
-  //definindo indicadores do inicio do jogo
-  myGame.frames += 1;
+    //definindo indicadores do inicio do jogo
+    myGame.frames += 1;
 
-  //checando se o deu gameOver
-  checkGameOver();
-  if (!myGame.stop) {
-    requestAnimationFrame(startGame);
-  }
+    //checando se o deu gameOver
+    checkGameOver();
+    if (!myGame.stop) {
+        requestAnimationFrame(startGame);
+    }
 
-  //mostrando score
-  myGame.updateScore();
-  myGame.showLives();
+    //mostrando score
+    myGame.updateScore();
+    myGame.showLives();
 
-  //fazendo o jogo ficar mais dificil
-  if (myGame.frames % 450 === 0) {
-    myGame.speed += 0.5;
-  }
+    //fazendo o jogo ficar mais dificil
+    if (myGame.frames % 450 === 0) {
+        myGame.speed += 0.5;
+    }
 }
 
 function checkGameOver() {
-  const crashed = myGame.obstacles.some((obstacle, index) => {
-    let check = myGame.player.crashWith(obstacle);
-    if (check) {
-      myGame.obstacles.splice(index, 1);
+    const crashed = myGame.obstacles.some((obstacle, index) => {
+        let check = myGame.player.crashWith(obstacle);
+        if (check) {
+            myGame.obstacles.splice(index, 1);
+        }
+        return check;
+    });
+    const fillPower = myGame.powers.some((power, index) => {
+        let getPower = myGame.player.crashWithLives(power);
+        if (getPower) {
+            myGame.powers.splice(index, 1);
+        }
+        return getPower;
+    });
+    if (crashed) {
+        myGame.lives -= 1;
+        if (myGame.lives === 0) {
+            myGame.stop = true;
+            myGame.audio.pause();
+            document.getElementById("stop-game").style.display = "flex";
+        } else {
+            myGame.stop = false;
+        }
     }
-    return check;
-  });
-  const fillPower = myGame.powers.some((power, index) => {
-    let getPower = myGame.player.crashWithLives(power);
-    if (getPower) {
-      myGame.powers.splice(index, 1);
+    if (fillPower) {
+        if (myGame.lives < 5) {
+            myGame.lives += 1;
+        }
     }
-    return getPower;
-  });
-  if (crashed) {
-    myGame.lives -= 1;
-    if (myGame.lives === 0) {
-      myGame.stop = true;
-      myGame.audio.pause();
-      document.getElementById("stop-game").style.display = "flex";
-    } else {
-      myGame.stop = false;
-    }
-  }
-  if (fillPower) {
-    if (myGame.lives < 5) {
-      myGame.lives += 1;
-    }
-  }
 }
